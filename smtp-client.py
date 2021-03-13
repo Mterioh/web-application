868611f358bf8271dc2b83e016fb52510f07a66f
Vyacheslav Shchurov <vschyurov@psystems.biz>
2021-03-13 01:01:12 -0500

    feat(smtp): added func interfaces

--- smtp-client.py
+++ smtp-client.py
@@ -8,3 +8,18 @@
 
 """
 import smtplib
+
+def main():
+    filename = input("Enter the name of message you will send:\n")
+    read_message_file(filename)
+
+def read_message_file(file):
+    return file
+
+def choose_recepients(file)
+
+def auth_smtp_client(smtp, host=465):
+
+
+if __name__ == '__main__':
+    main()
